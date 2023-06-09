e643d3c1d0bd85113db639075e69654c7b5b492d
Hoetty <kilian.behnke@outlook.com>
2023-06-09 21:15:49 +0200

    Updated to 1.19.3

--- src/main/java/com/hoetty/name/visibility/mixin/NameTagRenderer.java
+++ src/main/java/com/hoetty/name/visibility/mixin/NameTagRenderer.java
@@ -5,15 +5,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+
 import com.hoetty.name.visibility.Main;
 
 @Mixin(EntityRenderer.class)
 public abstract class NameTagRenderer {
 
-	@ModifyArgs(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLnet/minecraft/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I"))
-	private void modify(Args args) {
+    @ModifyArgs(method = "renderLabelIfPresent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I"))
+    private void modify(Args args) {
 		if (Main.NamesToggled){
     		args.set(3, 0xFFFFFFFF);
 		}
-	}	
+	}
 }
